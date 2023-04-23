ed90c7a0115ac873ff92071189a937acc5e7cc98
Sherif Kamal <sherif.kamal@startappz.com>
2023-04-23 13:38:26 +0200

    Initial Commit

---
+++ ForecastApp/ForecastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastAppApp.swift
+//  ForecastApp
+//
+//  Created by Sherif Kamal on 23/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ForecastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
