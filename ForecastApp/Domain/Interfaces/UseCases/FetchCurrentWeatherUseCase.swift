da4bccd9f071ea7c42c4fda03b4c093ddcf69aad
Sherif Kamal <sherif.kamal@startappz.com>
2023-04-27 21:24:40 +0200

    Create `DefaultFetchCurrentWeatherUseCase` as the default implementation for `FetchCurrentWeatherUseCase` interface

--- ForecastApp/Domain/Interfaces/UseCases/FetchCurrentWeatherUseCase.swift
+++ ForecastApp/Domain/Interfaces/UseCases/FetchCurrentWeatherUseCase.swift
@@ -10,3 +10,16 @@ import Foundation
 protocol FetchCurrentWeatherUseCase {
     func execute(searchType: SearchType) async throws -> Result<CurrentWeatherResponse>
 }
+
+
+final class DefaultFetchCurrentWeatherUseCase: FetchCurrentWeatherUseCase {
+    private let currentWeatherGateway: CurrentWeatherGateway
+    
+    init(currentWeatherGateway: CurrentWeatherGateway) {
+        self.currentWeatherGateway = currentWeatherGateway
+    }
+    
+    func execute(searchType: SearchType) async throws -> Result<CurrentWeatherResponse> {
+        return try await currentWeatherGateway.fetchCurrentWeather(searchType: searchType)
+    }
+}
