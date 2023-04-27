d9fb484e6fe6d98dfadfd06780f4acacfac429cc
Sherif Kamal <sherif.kamal@startappz.com>
2023-04-27 21:26:23 +0200

    Create `DefaultFetchWeatherDataUseCase` as the default implementation for `FetchWeatherDataUseCase` interface

--- ForecastApp/Domain/Interfaces/UseCases/FetchWeatherDataUseCase.swift
+++ ForecastApp/Domain/Interfaces/UseCases/FetchWeatherDataUseCase.swift
@@ -10,3 +10,17 @@ import Foundation
 protocol FetchWeatherDataUseCase {
     func execute(searchType: SearchType) async throws -> Result<ForecastResponse>
 }
+
+
+final class DefaultFetchRestaurantsUseCase: FetchWeatherDataUseCase {
+
+    private let weatherGateway: WeatherGateway
+    
+    init(weatherGateway: WeatherGateway) {
+        self.weatherGateway = weatherGateway
+    }
+    
+    func execute(searchType: SearchType) async throws -> Result<ForecastResponse> {
+        return try await weatherGateway.fetchWeatherData(searchType: searchType)
+    }
+}
