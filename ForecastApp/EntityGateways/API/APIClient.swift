b1744309ae9e2dfbe6408b3ab6c1c458de2c87f3
Sherif Kamal <sherif.kamal@startappz.com>
2023-04-27 21:32:55 +0200

    Create `ApiClientImplementation` as the implementation for APIClient for handling network implementation details

--- ForecastApp/EntityGateways/API/APIClient.swift
+++ ForecastApp/EntityGateways/API/APIClient.swift
@@ -20,3 +20,34 @@ protocol URLSessionProtocol {
 }
 
 extension URLSession: URLSessionProtocol {}
+
+class ApiClientImplementation: APIClient {
+    private let urlSession: URLSessionProtocol
+    
+    init(urlSession: URLSessionProtocol) {
+        self.urlSession = urlSession
+    }
+    
+    init(urlSessionConfiguration: URLSessionConfiguration, completionHandlerQueue: OperationQueue) {
+        self.urlSession = URLSession(configuration: urlSessionConfiguration, delegate: nil, delegateQueue: completionHandlerQueue)
+    }
+    
+    func execute<T>(request: APIRequest) async throws -> Result<APIResponse<T>> {
+        let (data, response) = try await urlSession.data(for: request.urlRequest)
+        guard let httpUrlResponse = response as? HTTPURLResponse else {
+            return .failure(NetworkRequestError(error: NSError(domain: "", code: 0)))
+        }
+        let successRange = 200...299
+        if successRange.contains(httpUrlResponse.statusCode) {
+            do {
+                let response = try APIResponse<T>(data: data, httpUrlResponse: httpUrlResponse)
+                return .success(response)
+            } catch {
+                return .failure(error)
+            }
+        } else {
+            return .failure(APIError(data: data, httpUrlResponse: httpUrlResponse))
+        }
+    }
+}
+
